bad7374c996417674346bff0f7933fe6050529d9
Kristian Skovgaard Osback <kristianskovgaard@gmail.com>
2020-10-03 12:58:47 +0200

    search works

--- ProgramFiles/SearchFunction.py
+++ ProgramFiles/SearchFunction.py
@@ -6,11 +6,10 @@ cardName = input('Enter part of name to find\n')
 #read csv, and split on "," the line
 csv_file = csv.reader(open('MunchkinTreasureCards.csv', "r"), delimiter=",")
 
-
 #loop through the csv list
 for row in csv_file:
+    if len(row) > 1: #checking for empty rows
     #if current rows 2nd value is equal to input, print that row
-    if cardName == row[1]:
-        print(row)
-    #else:
-     #   print("Nothing found!")
+        if cardName in row[1]:
+            print(row)
+        
