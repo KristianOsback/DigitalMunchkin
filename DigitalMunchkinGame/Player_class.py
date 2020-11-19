d6d59211241f4f6d3640e97ef22a0c40fdc0e7b1
Kristian Skovgaard Osback <kristianskovgaard@gmail.com>
2020-11-19 18:29:18 +0100

    before testing

--- DigitalMunchkinGame/Player_class.py
+++ DigitalMunchkinGame/Player_class.py
@@ -23,6 +23,10 @@ class Player:
         self.level = level
         self.levelTotal = levelTotal
         self.gold = gold
+        self.weaponBonus = weaponBonus
+        self.armourBonus = armourBonus
+        self.headgearBonus = headgearBonus
+        self.FootgearBonus = FootgearBonus
         print("(Initializing {})".format(self.handCards))
         print("(Initializing {})".format(self.tableCards))
         print("(Initializing {})".format(self.gender))
@@ -31,16 +35,47 @@ class Player:
         print("(Initializing {})".format(self.level))
         print("(Initializing {})".format(self.levelTotal))
         print("(Initializing {})".format(self.gold))
+        print("(Initializing {})".format(self.weaponBonus))
+        print("(Initializing {})".format(self.armourBonus))
+        print("(Initializing {})".format(self.headgearBonus))
+        print("(Initializing {})".format(self.FootgearBonus))
 
-    def CheckHandCards:
-        Armour bonus
+    def CheckHandCards():
         for card in self.handCards:
-            if card.cardType =  "Weapon":
-                if card.levelBonus > self.weaponBonus:
-                    for weaponCards in self.handCards:
-                        if card.cardType =  "Weapon":
-                            self.handCards.remove()
-                    self.handCards.append(card)
+            if card.cardType == "Weapon": #looking through hand cards searching for a weapon
+                if card.levelBonus > self.weaponBonus: #is it better than what you are wielding? 
+                    for weaponCard in self.tableCards: #looking on your table cards
+                        if weaponCard.cardType == "Weapon": #looking if your are wielding a weapon
+                            Cards_samlet.Cards.treasureCardsDiscardStack.append(weaponCard) #add it to discard stack
+                            self.tableCards.remove(weaponCard) #unwield weapon
+                            self.tableCards.append(card) #wield new weapon
+                            self.handCards.remove(card) #remove from handcards   
+            elif card.cardType == "Armour": 
+                if card.levelBonus > self.armourBonus: 
+                    for armourCard in self.tableCards: 
+                        if armourCard.cardType == "Armour": 
+                            Cards_samlet.Cards.treasureCardsDiscardStack.append(armourCard) 
+                            self.tableCards.remove(armourCard) 
+                            self.tableCards.append(card) 
+                            self.handCards.remove(card) 
+            elif card.cardType == "Headgear": 
+                if card.levelBonus > self.headgearBonus: 
+                    for headgearCard in self.tableCards: 
+                        if headgearCard.cardType == "Headgear": 
+                            Cards_samlet.Cards.treasureCardsDiscardStack.append(headgearCard) 
+                            self.tableCards.remove(headgearCard)
+                            self.tableCards.append(card) 
+                            self.handCards.remove(card) 
+            elif card.cardType == "Footgear": 
+                if card.levelBonus > self.footgearBonus:
+                    for footgearCard in self.tableCards: 
+                        if footgearCard.cardType == "Footgear": 
+                            Cards_samlet.Cards.treasureCardsDiscardStack.append(footgearCard)
+                            self.tableCards.remove(footgearCard) 
+                            self.tableCards.append(card) 
+                            self.handCards.remove(card)
+            else
+                print("No gear cards")
                     
                     
                     
